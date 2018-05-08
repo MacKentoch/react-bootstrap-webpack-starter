ad0d8ebe41cf12595d5fe877e7a3f156e66fb27d
MacKentoch <erwan.datin@gmail.com>
2018-05-08 07:03:51 +0200

    UTest

---
+++ src/front/pages/login/__tests__/Login.test.js
@@ -0,0 +1,26 @@
+// @flow
+
+// #region imports
+import React from 'react';
+import renderer from 'react-test-renderer'; // needed both for snpashot testing but also to prevent errors from enzyme
+import { MemoryRouter } from 'react-router';
+import Login from '../Login';
+// #endregion
+
+describe('Login page', () => {
+  it('renders as expected', () => {
+    const props = {
+      actions: {},
+    };
+    const component = renderer
+      .create(
+        <div>
+          <MemoryRouter>
+            <Login {...props} />
+          </MemoryRouter>
+        </div>,
+      )
+      .toJSON();
+    expect(component).toMatchSnapshot();
+  });
+});
