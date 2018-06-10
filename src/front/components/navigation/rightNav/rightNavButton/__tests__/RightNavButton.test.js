0df222782a024cd1d11c2c33cffa72ee90081f7e
MacKentoch <erwan.datin@gmail.com>
2018-06-10 08:44:10 +0200

    tests

--- src/front/components/navigation/rightNav/rightNavButton/__tests__/RightNavButton.test.js
+++ src/front/components/navigation/rightNav/rightNavButton/__tests__/RightNavButton.test.js
@@ -4,10 +4,10 @@
 import React from 'react';
 import renderer from 'react-test-renderer'; // needed both for snpashot testing but also to prevent errors from enzyme
 import { MemoryRouter } from 'react-router';
-import LeftNavButton from '../LeftNavButton';
+import RightNavButton from '../RightNavButton';
 // #endregion
 
-describe('LeftNavButton component', () => {
+describe('RightNavButton component', () => {
   it('renders as expected', () => {
     const props = {
       link: '/',
@@ -20,7 +20,7 @@ describe('LeftNavButton component', () => {
       .create(
         <div>
           <MemoryRouter>
-            <LeftNavButton {...props} />
+            <RightNavButton {...props} />
           </MemoryRouter>
         </div>,
       )
