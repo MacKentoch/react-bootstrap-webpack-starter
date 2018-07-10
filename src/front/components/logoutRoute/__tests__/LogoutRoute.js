046dc162fd6d5a260176de42ec6b46c753f4a1fb
MacKentoch <erwan.datin@gmail.com>
2018-07-10 06:25:00 +0200

    tests

---
+++ src/front/components/logoutRoute/__tests__/LogoutRoute.js
@@ -0,0 +1,25 @@
+// @flow
+
+// #region imports
+import React from 'react';
+import renderer from 'react-test-renderer'; // needed both for snpashot testing but also to prevent errors from enzyme
+import { MemoryRouter } from 'react-router';
+import LogoutRoute from '../LogoutRoute';
+// #endregion
+
+describe('LogoutRoute component', () => {
+  it('renders as expected', () => {
+    const props = {
+      disconnectUser: () => {},
+    };
+
+    const component = renderer
+      .create(
+        <MemoryRouter>
+          <LogoutRoute {...props} />
+        </MemoryRouter>,
+      )
+      .toJSON();
+    expect(component).toMatchSnapshot();
+  });
+});
