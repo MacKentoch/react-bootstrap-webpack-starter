a366ce14b85a6cc3c858bd2c8032375f23f45fe7
MacKentoch <erwan.datin@gmail.com>
2018-07-19 06:30:49 +0200

    tests

--- src/front/services/auth/__tests__/auth.test.js
+++ src/front/services/auth/__tests__/auth.test.js
@@ -5,13 +5,17 @@ import auth from '../index';
 // #endregion
 
 describe('auth service', () => {
+  it('localStorage should exist', () => {
+    expect(localStorage).toBeDefined();
+  });
+
   it('should set token to localStorage (default store)', () => {
     const value = 'something to save';
     const tokenKey = 'test';
 
     // set token
     auth.setToken(value, null, tokenKey);
-    const returnValue = auth.getToken(tokenKey);
+    const returnValue = auth.getToken(null, tokenKey);
     expect(returnValue).toBe(value);
   });
 });
