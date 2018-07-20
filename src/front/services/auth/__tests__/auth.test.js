2fc514977f296c11d4913aba82df6b5f09f3300f
MacKentoch <erwan.datin@gmail.com>
2018-07-20 06:18:41 +0200

    tests

--- src/front/services/auth/__tests__/auth.test.js
+++ src/front/services/auth/__tests__/auth.test.js
@@ -14,8 +14,9 @@ describe('auth service', () => {
     const tokenKey = 'test';
 
     // set token
-    auth.setToken(value, null, tokenKey);
-    const returnValue = auth.getToken(null, tokenKey);
-    expect(returnValue).toBe(value);
+    auth.setToken(value, 'localStorage', tokenKey);
+    expect(localStorage.setItem).toHaveBeenLastCalledWith(tokenKey, value);
+    // const returnValue = auth.getToken(null, tokenKey);
+    // expect(returnValue).toBe(value);
   });
 });
--- src/test/setupTests.js
+++ src/test/setupTests.js
@@ -2,6 +2,7 @@
 
 // to avoid: Warning: React depends on requestAnimationFrame. Make sure that you load a polyfill in older browsers. http://fb.me/react-polyfills
 require('raf/polyfill');
+require('jest-localstorage-mock');
 
 const Enzyme = require('enzyme');
 const EnzymeAdapter = require('enzyme-adapter-react-16');
