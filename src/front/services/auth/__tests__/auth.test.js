e3a45974ecf7ce20c23ac0df578187c2e42f15ac
MacKentoch <erwan.datin@gmail.com>
2018-07-21 08:08:58 +0200

    tests

--- src/front/services/auth/__tests__/auth.test.js
+++ src/front/services/auth/__tests__/auth.test.js
@@ -16,7 +16,13 @@ describe('auth service', () => {
     // set token
     auth.setToken(value, 'localStorage', tokenKey);
     expect(localStorage.setItem).toHaveBeenLastCalledWith(tokenKey, value);
-    // const returnValue = auth.getToken(null, tokenKey);
-    // expect(returnValue).toBe(value);
+  });
+
+  it('should get token from localStorage (default store)', () => {
+    const tokenKey = 'test';
+
+    // set token
+    auth.getToken('localStorage', tokenKey);
+    expect(localStorage.getItem).toHaveBeenLastCalledWith(tokenKey);
   });
 });
