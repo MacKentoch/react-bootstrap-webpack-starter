d812c3033699192f763db16140f177c2a1e65106
MacKentoch <erwan.datin@gmail.com>
2018-07-27 06:25:02 +0200

    Login and PageNotfound pages plitted

--- src/front/Root.js
+++ src/front/Root.js
@@ -4,6 +4,7 @@
 import React, { Component } from 'react';
 import { Router, Switch, Route } from 'react-router-dom';
 import compose from 'recompose/compose';
+import loadable from 'loadable-components';
 // #region import createHistory from hashHistory or BrowserHistory:
 // import createHistory from 'history/createHashHistory';
 import createHistory from 'history/createBrowserHistory';
@@ -25,6 +26,9 @@ type State = any;
 // #region constants
 const MainApp = compose(withMainLayout())(MainRoutes);
 const history = createHistory();
+// we lazy load pages:
+const LoadableLogin = loadable(() => import('./pages/login'));
+const LoadablePageNotFound = loadable(() => import('./pages/pageNotFound'));
 // #endregion
 
 class Root extends Component<Props, State> {
@@ -34,12 +38,12 @@ class Root extends Component<Props, State> {
         <AuthProvider>
           <ScrollToTop>
             <Switch>
-              <Route exact path="/login" component={Login} />
+              <Route exact path="/login" component={LoadableLogin} />
               {/* Application with main layout (could have multiple applications with different layouts) */}
               <MainApp />
               {/* logout: just redirects to login (App will take care of removing the token) */}
               <LogoutRoute path="/logout" />
-              <Route component={PageNotFound} />
+              <Route component={LoadablePageNotFound} />
             </Switch>
           </ScrollToTop>
         </AuthProvider>
