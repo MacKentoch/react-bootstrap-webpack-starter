118c546df7dce36e184272075b4321cc2df87e43
MacKentoch <erwan.datin@gmail.com>
2019-11-05 05:54:00 +0100

    migration

---
+++ front/jest.config.js
@@ -0,0 +1,26 @@
+const { jsWithBabel: tsjPreset } = require('ts-jest/presets');
+
+module.exports = {
+  preset: 'ts-jest',
+  transform: { ...tsjPreset.transform },
+  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  globals: {
+    'ts-jest': {
+      tsConfig: './tsconfig.json',
+      babelConfig: false,
+    },
+  },
+  testEnvironment: 'node',
+  verbose: true,
+  roots: ['<rootDir>/src/', '<rootDir>/test'],
+  setupFiles: [],
+  setupFilesAfterEnv: ['<rootDir>/test/setupTests.ts'],
+  moduleNameMapper: {
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+      '<rootDir>/src/test/__mocks__/fileMock.js',
+    '\\.(css|less|sass|scss)$': 'identity-obj-proxy',
+  },
+  coverageDirectory: './coverage/',
+  collectCoverage: true,
+};
