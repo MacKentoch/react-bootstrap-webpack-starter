26884e59b7c8025489d3ac974deb98aefcbf03b0
MacKentoch <erwan.datin@gmail.com>
2019-11-18 05:27:00 +0100

    migration

--- front/src/contexts/auth/consumerHOC/index.tsx
+++ front/src/contexts/auth/consumerHOC/index.tsx
@@ -1,19 +1,15 @@
-// @flow
-
-// #region imports
 import React, { Component } from 'react';
 import wrapDisplayName from 'recompose/wrapDisplayName';
 import { AuthContextConsumer } from '../context/index';
-import { type AuthProviderState } from '../providerComponent';
-// #endregion
+import { AuthProviderState } from '../providerComponent';
 
 // #region flow types
-export type AuthContextProps = { ...any } & AuthProviderState;
+export type AuthContextProps = {} & AuthProviderState;
 // #endregion
 
 // #region CONSUMER HOC
 export default function withAuth(/* additionnal args if needed */) {
-  return BaseComponent => {
+  return (BaseComponent: React.Component<any, any>) => {
     class WithAuth extends Component<any, any> {
       render() {
         const { ...passProps } = this.props;
@@ -31,6 +27,7 @@ export default function withAuth(/* additionnal args if needed */) {
     // #region add static displayName for dev
     /* eslint-disable no-process-env */
     if (process.env.NODE_ENV !== 'production') {
+      // @ts-ignore
       WithAuth.displayName = wrapDisplayName(BaseComponent, 'WithAuth');
     }
     /* eslint-enable no-process-env */
