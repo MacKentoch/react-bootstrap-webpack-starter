c18917933061f01057b9885ee34aa80a3f7ed749
MacKentoch <erwan.datin@gmail.com>
2020-01-26 07:14:39 +0100

    migration

--- front/src/components/loadingContent/__tests__/LoadingContent.test.tsx
+++ front/src/components/loadingContent/__tests__/LoadingContent.test.tsx
@@ -1,10 +1,23 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { render } from '@testing-library/react';
+import '@testing-library/react/cleanup-after-each';
 import LoadingContent from '../LoadingContent';
 
+let rootElement: any = null;
+
 describe('LoadingContent component', () => {
+  beforeEach(() => {
+    rootElement = document.createElement('div');
+    document.body.appendChild(rootElement);
+  });
+
+  afterEach(() => {
+    rootElement && document.body.removeChild(rootElement);
+    rootElement = null;
+  });
+
   it('renders as expected', () => {
-    const component = shallow(<LoadingContent />);
-    expect(component).toMatchSnapshot();
+    const { container } = render(<LoadingContent />, rootElement);
+    expect(container.firstChild).toMatchSnapshot();
   });
 });
