5b75f39c473accdb569b7603fa6154df7c4cad55
MacKentoch <erwan.datin@gmail.com>
2020-02-02 07:39:07 +0100

    migration

--- front/src/components/mainLayout/__tests__/MainLayout.test.tsx
+++ front/src/components/mainLayout/__tests__/MainLayout.test.tsx
@@ -1,18 +1,32 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { render } from '@testing-library/react';
 import { MemoryRouter } from 'react-router';
+import '@testing-library/react/cleanup-after-each';
+import { AuthProvider } from '../../../contexts/auth';
 import MainLayout from '../MainLayout';
 
+let rootElement: any = null;
+
 describe('MainLayout component', () => {
+  beforeEach(() => {
+    rootElement = document.createElement('div');
+    document.body.appendChild(rootElement);
+  });
+
+  afterEach(() => {
+    rootElement && document.body.removeChild(rootElement);
+    rootElement = null;
+  });
+
   it('renders as expected', () => {
-    const component = shallow(
+    const { container } = render(
       <MemoryRouter>
         <MainLayout>
           <p>children here</p>
         </MainLayout>
       </MemoryRouter>,
+      rootElement,
     );
-
-    expect(component).toMatchSnapshot();
+    expect(container.firstChild).toMatchSnapshot();
   });
 });
