1df33dc37dd7d5d0a43effcc1ad355747ddef13e
MacKentoch <erwan.datin@gmail.com>
2020-02-06 05:23:56 +0100

    migration

--- front/src/pages/login/__tests__/Login.test.tsx
+++ front/src/pages/login/__tests__/Login.test.tsx
@@ -1,24 +1,36 @@
 import React from 'react';
-import { shallow } from 'enzyme';
 import { MemoryRouter } from 'react-router';
+import { render } from '@testing-library/react';
+import '@testing-library/react/cleanup-after-each';
 import Login from '../Login';
 
+let rootElement: any = null;
+
 describe('Login page', () => {
+  beforeEach(() => {
+    rootElement = document.createElement('div');
+    document.body.appendChild(rootElement);
+  });
+
+  afterEach(() => {
+    rootElement && document.body.removeChild(rootElement);
+    rootElement = null;
+  });
+
   it('renders as expected', () => {
     const props = {
-      disconnectUser: () => {},
-      setToken: () => {},
-      setUserInfo: () => {},
-      checkIsAuthenticated: () => {},
+      disconnectUser: jest.fn(),
+      setToken: jest.fn(),
+      setUserInfo: jest.fn(),
+      checkIsAuthenticated: jest.fn(),
     };
 
-    const component = shallow(
-      <div>
-        <MemoryRouter>
-          <Login {...props} />
-        </MemoryRouter>
-      </div>,
+    const { container } = render(
+      <MemoryRouter>
+        <Login {...props} />
+      </MemoryRouter>,
+      rootElement,
     );
-    expect(component).toMatchSnapshot();
+    expect(container.firstChild).toMatchSnapshot();
   });
 });
