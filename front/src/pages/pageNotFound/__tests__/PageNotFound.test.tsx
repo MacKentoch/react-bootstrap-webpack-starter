fbeef0d62a9e52246ff18b99fb0b632beb6e5ed1
MacKentoch <erwan.datin@gmail.com>
2020-02-07 05:29:37 +0100

    migration

--- front/src/pages/pageNotFound/__tests__/PageNotFound.test.tsx
+++ front/src/pages/pageNotFound/__tests__/PageNotFound.test.tsx
@@ -1,17 +1,29 @@
 import React from 'react';
-import { shallow } from 'enzyme';
 import { MemoryRouter } from 'react-router';
+import { render } from '@testing-library/react';
+import '@testing-library/react/cleanup-after-each';
 import PageNotFound from '../PageNotFound';
 
+let rootElement: any = null;
+
 describe('PageNotFound page', () => {
+  beforeEach(() => {
+    rootElement = document.createElement('div');
+    document.body.appendChild(rootElement);
+  });
+
+  afterEach(() => {
+    rootElement && document.body.removeChild(rootElement);
+    rootElement = null;
+  });
+
   it('renders as expected', () => {
-    const component = shallow(
-      <div>
-        <MemoryRouter>
-          <PageNotFound />
-        </MemoryRouter>
-      </div>,
+    const { container } = render(
+      <MemoryRouter>
+        <PageNotFound />
+      </MemoryRouter>,
+      rootElement,
     );
-    expect(component).toMatchSnapshot();
+    expect(container.firstChild).toMatchSnapshot();
   });
 });
